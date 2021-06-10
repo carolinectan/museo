73fd15e268e6a3285882d719db07a05c37f41787
Caroline Tan <carolinectan3@gmail.com>
2021-06-10 15:58:59 -0400

    Add iteration 3 tests

--- spec/curator_spec.rb
+++ spec/curator_spec.rb
@@ -65,4 +65,81 @@ RSpec.describe Curator do
       expect(@curator.find_artist_by_id("1")).to eq(@artist_1)
     end
   end
+
+  describe 'iteration 3' do
+    before :each do
+      @curator = Curator.new
+      @photo_1 = Photograph.new({
+                                   id: "1",
+                                   name: "Rue Mouffetard, Paris (Boy with Bottles)",
+                                   artist_id: "1",
+                                   year: "1954"
+                                 })
+      @photo_2 = Photograph.new({
+                                   id: "2",
+                                   name: "Moonrise, Hernandez",
+                                   artist_id: "2",
+                                   year: "1941"
+                                 })
+      @photo_3 = Photograph.new({
+                                   id: "3",
+                                   name: "Identical Twins, Roselle, New Jersey",
+                                   artist_id: "3",
+                                   year: "1967"
+                                 })
+      @photo_4 = Photograph.new({
+                                   id: "4",
+                                   name: "Monolith, The Face of Half Dome",
+                                   artist_id: "3",
+                                   year: "1927"
+                                 })
+      @artist_1 = Artist.new({
+                                id: "1",
+                                name: "Henri Cartier-Bresson",
+                                born: "1908",
+                                died: "2004",
+                                country: "France"
+                              })
+      @artist_2 = Artist.new({
+                                id: "2",
+                                name: "Ansel Adams",
+                                born: "1902",
+                                died: "1984",
+                                country: "United States"
+                              })
+      @artist_3 = Artist.new({
+                                id: "3",
+                                name: "Diane Arbus",
+                                born: "1923",
+                                died: "1971",
+                                country: "United States"
+                              })
+      @curator.add_artist(@artist_1)
+      @curator.add_artist(@artist_2)
+      @curator.add_artist(@artist_3)
+
+      @curator.add_photograph(@photo_1)
+      @curator.add_photograph(@photo_2)
+      @curator.add_photograph(@photo_3)
+      @curator.add_photograph(@photo_4)
+    end
+
+    it 'can return photographs by artist' do
+      expected = {
+                    @artist_1 => [@photo_1],
+                    @artist_2 => [@photo_2],
+                    @artist_3 => [@photo_3, @photo_4]
+                  }
+      expect(@curator.photographs_by_artist).to eq(expected)
+    end
+
+    it 'can return artists with multiple photographs' do
+      expect(@curator.artists_with_multiple_photographs).to eq(["Diane Arbus"])
+    end
+
+    it 'can return photographs taken by an artist from a specific country' do
+      expect(@curator.photographs_taken_by_artist_from("United States")).to eq([@photo_2, @photo_3, @photo_4)
+      expect(@curator.photographs_taken_by_artist_from("Argentina")).to eq([])
+    end
+  end
 end
