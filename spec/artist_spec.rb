8a8bd649bda85972f51510859258f9ee12ffe0c6
Caroline Tan <carolinectan3@gmail.com>
2021-06-10 15:36:01 -0400

    Add it exists tests

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -2,26 +2,32 @@ require 'rspec'
 require './lib/artist'
 
 RSpec.describe Artist do
-  before :each do
-    attributes = {
-                    id: "2",
-                    name: "Ansel Adams",
-                    born: "1902",
-                    died: "1984",
-                    country: "United States"
-                  }
-    @artist = Artist.new(attributes)
-  end
+  describe 'iteration 1' do
+    before :each do
+      attributes = {
+                      id: "2",
+                      name: "Ansel Adams",
+                      born: "1902",
+                      died: "1984",
+                      country: "United States"
+                    }
+      @artist = Artist.new(attributes)
+    end
 
-  it 'initializes with attributes' do
-    expect(@artist.id).to eq("2")
-    expect(@artist.name).to eq("Ansel Adams")
-    expect(@artist.born).to eq("1902")
-    expect(@artist.died).to eq("1984")
-    expect(@artist.country).to eq("United States")
-  end
+    it 'exists' do
+      expect(@artist).to be_a(Artist)
+    end
 
-  it 'can return age at death' do
-    expect(@artist.age_at_death).to eq(82)
+    it 'initializes with attributes' do
+      expect(@artist.id).to eq("2")
+      expect(@artist.name).to eq("Ansel Adams")
+      expect(@artist.born).to eq("1902")
+      expect(@artist.died).to eq("1984")
+      expect(@artist.country).to eq("United States")
+    end
+
+    it 'can return age at death' do
+      expect(@artist.age_at_death).to eq(82)
+    end
   end
 end
--- spec/photograph_spec.rb
+++ spec/photograph_spec.rb
@@ -2,20 +2,26 @@ require 'rspec'
 require './lib/photograph'
 
 RSpec.describe Photograph do
-  before :each do
-    attributes = {
-                    id: "1",
-                    name: "Rue Mouffetard, Paris (Boy with Bottles)",
-                    artist_id: "4",
-                    year: "1954"
-                  }
-    @photograph = Photograph.new(attributes)
-  end
+  describe 'iteration 1' do
+    before :each do
+      attributes = {
+                      id: "1",
+                      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+                      artist_id: "4",
+                      year: "1954"
+                    }
+      @photograph = Photograph.new(attributes)
+    end
 
-  it 'initializes with attributes' do
-    expect(@photograph.id).to eq("1")
-    expect(@photograph.name).to eq("Rue Mouffetard, Paris (Boy with Bottles)")
-    expect(@photograph.artist_id).to eq("4")
-    expect(@photograph.year).to eq("1954")
+    it 'exists' do
+      expect(@photograph).to be_a(Photograph)
+    end
+
+    it 'initializes with attributes' do
+      expect(@photograph.id).to eq("1")
+      expect(@photograph.name).to eq("Rue Mouffetard, Paris (Boy with Bottles)")
+      expect(@photograph.artist_id).to eq("4")
+      expect(@photograph.year).to eq("1954")
+    end
   end
 end
