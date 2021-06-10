78659da07e803142c15b4adf802e3b63fddc055b
Caroline Tan <carolinectan3@gmail.com>
2021-06-10 15:13:34 -0400

    Create artist tests

---
+++ spec/artist_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require './lib/artist'
+
+RSpec.describe Artist do
+  before :each do
+    attributes = {
+                    id: "2",
+                    name: "Ansel Adams",
+                    born: "1902",
+                    died: "1984",
+                    country: "United States"
+                  }
+    @artist = Artist.new(attributes)
+  end
+
+  it 'initializes with attributes' do
+    expect(@artist.id).to eq("2")
+    expect(@artist.name).to eq("Ansel Adams")
+    expect(@artist.born).to eq("1902")
+    expect(@artist.died).to eq("1984")
+    expect(@artist.country).to eq("United States")
+    expect(@artist.age_at_death).to eq(82)
+  end
+end
