6f149c3e4810f4a27f66b6f1ab8915aee425174b
Caroline Tan <carolinectan3@gmail.com>
2021-06-10 15:42:15 -0400

    Create curator class and methods

---
+++ lib/curator.rb
@@ -0,0 +1,22 @@
+class Curator
+  attr_reader :photographs, :artists
+
+  def initialize
+    @photographs = []
+    @artists = []
+  end
+
+  def add_photograph(photograph)
+    @photographs << photograph
+  end
+
+  def add_artist(artist)
+    @artists << artist
+  end
+
+  def find_artist_by_id(id) #id argument is a string
+    @artists.find do |artist|
+      artist.id == id
+    end
+  end
+end
