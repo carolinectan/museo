759a190be935caa7b3b75491598a9a5810f31d33
Caroline Tan <carolinectan3@gmail.com>
2021-06-10 15:21:43 -0400

    Refactor tests

---
+++ lib/photograph.rb
@@ -0,0 +1,11 @@
+class Photograph
+  attr_reader :id, :name, :artist_id, :year
+
+  def initialize(attributes)
+    @id = attributes[:id]
+    @name = attributes[:name]
+    @artist_id = attributes[:artist_id]
+    @year = attributes[:year]
+  end
+
+end
--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -19,6 +19,9 @@ RSpec.describe Artist do
     expect(@artist.born).to eq("1902")
     expect(@artist.died).to eq("1984")
     expect(@artist.country).to eq("United States")
+  end
+
+  it 'can return age at death' do
     expect(@artist.age_at_death).to eq(82)
   end
 end
